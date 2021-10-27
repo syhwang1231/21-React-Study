ab6222101f0c37db5f84cbb55e783c7317d89da7
syhwang1231 <syhhwang1231@gmail.com>
2021-10-27 21:34:42 +0900

    Section 2 리액트와 비동기 리퀘스트, Section 3

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCO5FpDRgUwn6zIFkRKt00M0BooArtz6mI';
+
 
 // Create a new component.
 // This component should produce some HTML
-const App = () => {
-	return (
-	<div>
-		<SearchBar />
-	</div>
-	);
+class App extends Component {
+	constructor(props){
+		super(props);
+		
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+		
+		this.videoSearch('surfboards');
+	}
+	
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+			// this.setState({videos: videos}); 와 동일.
+			// key, value의 이름이 같을 때만 사용.
+		});
+	}
+	
+	render(){
+		const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
+			
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+					videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 // 함수는 실제 DOM에 렌더링되는 컴포넌트의 인스턴스들을 만든다.
