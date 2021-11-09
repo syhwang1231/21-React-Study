62b9438d3562db0456a9d982e9f4d5535e33ee82
Suyeon <syhhwang1231@gmail.com>
2021-11-10 04:05:42 +0900

    Section5 ~End

---
+++ ReduxSimpleWeather/.metadata/.plugins/org.eclipse.core.resources/.history/a2/40b526572641001c12a4eb2d16c45bf9
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import WeatherReducer from './reducer_weather';
+
+const rootReducer = combineReducers({
+  state: (state = {}) => state
+});
+
+export default rootReducer;
---
+++ ReduxSimpleWeather/ReduxSimpleWeather/src/reducers/reducer_weather.js
@@ -0,0 +1,10 @@
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action){
+	switch (action.type) {
+	case FETCH_WEATHER:
+		return state.concat([ action.payload.data ]);  // returning a new version of state
+		//return [ action.payload.data, ...state ];  // [city, city, city]
+	}
+	return state;
+}
